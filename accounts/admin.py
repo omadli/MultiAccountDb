b6c2714fdc730e313f7a7d4140d9d0bd77a965c1
Omadli Yigit <76625693+omadli@users.noreply.github.com>
2024-03-31 01:38:44 +0500

    Added admin panel Accounts view

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Account, Group, Channel
 
-admin.site.register(Account)
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'folder', 'phone', 'user_id', 'first_name', 'last_name', 'username', 'is_spam')
+    list_display_links = ('id', 'user_id', 'first_name', 'last_name')
+    
+    search_fields = ('user_id', 'phone', 'first_name', 'last_name', 'username')
+    list_filter = ('is_spam', 'folder')
+    show_facets = admin.ShowFacets.ALWAYS
+    
+    @admin.display(description="Telefon raqami", ordering='phone_number')
+    def phone(self, obj: Account):
+        return f"+{obj.phone_number}"
+    
 admin.site.register(Group)
 admin.site.register(Channel)
